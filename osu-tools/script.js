ef01f8ceaa762a4f95ba71366a0116162a3281dc
Rekunan <Mangsn05@gmail.com>
2023-08-04 01:51:21 -0500

    Remove `Calculate` button & calc automatically

--- osu-tools/script.js
+++ osu-tools/script.js
@@ -1,12 +1,17 @@
-function NoteDensity(AR, BPM) {
-  return Preempt(AR) / 3 * BPM / 10000;
-}
+var AR = document.querySelector('#AR');
+var BPM = document.querySelector('#BPM');
+var density = document.querySelector('#Density');
 
-function Preempt(AR) {
+function noteDensity(AR, BPM) { return preempt(AR) / 3 * BPM / 10000; }
+
+function preempt(AR) {
   if (AR < 5) { return 1200 + 600 * (5 - AR) / 5; }
   else { return 1200 - 750 * (AR - 5) / 5; }
 }
 
-function densityFunc() {
-  document.getElementById("Density").innerHTML = NoteDensity(parseFloat(document.getElementById("AR").value), parseFloat(document.getElementById("BPM").value));
-}
+function densityFunc() { density.textContent = noteDensity(AR.value || 0, BPM.value || 0); }
+
+densityFunc(); // Run once at startup
+
+AR.addEventListener('input', densityFunc);
+BPM.addEventListener('input', densityFunc);
